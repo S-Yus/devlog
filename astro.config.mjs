d10f554b5402f0c55345d24d7f5a0bcf7920301c
S-Yus <sa2yusei1030@icloud.com>
2026-08-10 10:38:25 +0900

    Add LaTeX math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import { unified } from '@astrojs/markdown-remark';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   site: 'https://yusei.dev',
   output: 'static',
   integrations: [sitemap()],
   markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
     shikiConfig: {
       theme: 'github-dark-default',
       wrap: true,
